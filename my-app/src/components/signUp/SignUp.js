dcd7abfb1895d12ec1d84c00735d703772d2b7dc
Anna Bavtruk <ann.bavtruk@gmail.com>
2021-10-03 21:03:03 +0300

    add icons

--- my-app/src/components/signUp/SignUp.js
+++ my-app/src/components/signUp/SignUp.js
@@ -1,14 +1,29 @@
 import React from 'react'
 import './SignUp.css'
-import { farBars } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { farBars } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { farBars } from "react-icons/fa";
+import { FaBars } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';
+import { FaClipboard } from 'react-icons/fa';
+import { FaCog } from 'react-icons/fa';
 
 export default function SignUp(){
     return(
 
     <div>
         <nav className='navigation'>
-        <FontAwesomeIcon icon={farBars} />
+        <div className='icons'>
+        <FaBars />
+        <FaHome />
+        <FaUser />
+        <FaStar />
+        <FaClipboard />
+        <FaCog />
+        </div>
+
         </nav>
     </div>
     )
