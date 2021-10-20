8f7a19db49ca299d5e4a0b8c2eddd76f7cb259e5
Anna Bavtruk <ann.bavtruk@gmail.com>
2021-10-20 20:58:15 +0300

    add isAuth to search.js

--- my-app/src/components/signUp/search.js
+++ my-app/src/components/signUp/search.js
@@ -5,10 +5,11 @@ import { FaCog } from 'react-icons/fa';
 import {FaSearch} from 'react-icons/fa';
 
 export default function Search(){
+    const isAuth = useSelector(state => state.user.isAuth)
     return(
 <div className='search'>
         <div><img className='av' src='/images/avatar.png'/></div>
-        <div><h3 className='name'>Tomasz Kowalski</h3></div>
+        {<div><h3 className='name'>isAuth</h3></div>}
         <div className='inputSearch'>
             {/* <img classNme='imgSearch' src='/images/search.png'/> */}
             <FaSearch className='imgSearch'/>
