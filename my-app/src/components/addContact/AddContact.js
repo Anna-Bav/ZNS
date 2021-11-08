a45a89989389db665799165217cd6f878e95ab5e
Anna Bavtruk <ann.bavtruk@gmail.com>
2021-11-08 21:03:05 +0300

    add select and textarea

--- my-app/src/components/addContact/AddContact.js
+++ my-app/src/components/addContact/AddContact.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './AddContact.css'
 
 export default function AddContact(){
-    return (
-        <div></div>
+    const [value, setValue] = useState('');
+
+    function changeSelect(e) {
+        setValue(e.target.value);
+    }
+
+   return (
+
+        <div className='addContact'>
+            <div className='addContact_1'>
+            <img src='/images/addContact.png'/>
+           
+            </div>
+            <div className='addContact_2'>
+                <div className='addNewContact'>
+                    <div className='newContactTitle'>Add new contact</div>
+                    <div className='newContactText'>
+                        <h5>Basic</h5>
+                        <h5>Additionally</h5>
+                    </div>
+                    <div className='addFirstName'>
+                        <div className='addInputText'>First name</div>
+                        <div><input className='addInfo' name='name' type='text' placeholder='Enter first name'/></div>
+                    </div >
+                    <div className='addFirstName'>
+                    <div className='addInputText'>Last name</div>
+                        <div><input className='addInfo' name='name' type='text' placeholder='Enter last name'/></div>
+                    </div>
+                    <div className='addPhone'>
+                    <div className='addInputText'>Phone</div>
+                    <div><input className='addInfo' name='name' type='phone' placeholder='+ 375 29 123 45 67'/></div>
+                    <div>
+                        <select className='select' value={value} onChange={changeSelect}>
+                            <option value='home'>Home</option>
+                            <option value='work'>Work</option>
+                            <option value='office'>Office</option>
+                            <option value='plus'><img src='/images/plus_select.png'/></option>
+                        </select>
+                    </div>
+                    </div>
+                    <div className='addPhone'>
+                    <div className='addInputText'>E-mail</div>
+                    <div><input className='addInfo' name='name' type='phone' placeholder='Enter e-mail address'/></div>
+                    <div>
+                    <select className='select' value={value} onChange={changeSelect}>
+                            <option value='home'>Home</option>
+                            <option value='work'>Work</option>
+                            <option value='office'>Office</option>
+                            <option value='plus'><img src='/images/plus_select.png'/></option>
+                        </select>
+                    </div>
+                    </div>
+                    <div className='addTextarea'>
+                    <div className='addInputText'>Notes</div>
+                    <div><input className='textarea' name='taxtValue' type='textarea' placeholder='Enter your notes'/></div>
+                    </div>
+                    <div className='button_addContact'> 
+                    Add contact
+                    </div>
+                </div>
+                <div className='addPhoto'></div>
+            </div>
+        </div>
     )
+    
 }
